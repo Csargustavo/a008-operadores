38aef925c7fd02040415280f200793847ea68b07
Cesar Gustavo <csargustavo.cg@gmail.com>
2022-07-21 21:14:42 -0300

    exercicios aula 008

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const numeroa = Number (prompt("Insira um numero Par"))
+
+console.log("divide por 2", numeroa %2)
+
+// por padão numeros pares ficam igual a 0 e impares ficam igual a 1
---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+console.log ("exercicio 3");
+    
+const numeroa = 5 > 20 && 5 < 2
+console.log (numeroa)
+
+const numerob = 5 === 5 || 5 === "5"
+console.log (numerob)
+
+const numeroc = 120>50
+console.log (numeroc)
+
+const numerod = !(20>50)
+console.log (numerod)
